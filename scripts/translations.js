99aac3fd5fbbe2093a7fb7d4ea14adc7932f3c77
Krasimir Hristov <125263294+krasimir-hr@users.noreply.github.com>
2025-09-29 19:16:25 +0300

    Add files via upload

---
+++ scripts/translations.js
@@ -0,0 +1,228 @@
+const translations = {
+  en: {
+    "nav-home-btn": "Home",
+    "nav-service-btn": "Services",
+    "nav-about-btn": "About us",
+
+    "hero-1": "For the easy moments, there are many.",
+    "hero-2": "For the challenging moments, you have us.",
+    "hero-3": "Competent humanity for every stage of your personal and professional life.",
+    "hero-4": "people, persons, needs",
+    "hero-5": "Never alone again.",
+    "hero-6": "Discover Our Services ",
+
+    "services-header": "OUR SERVCIES",
+    "services-under-header": "Competent humanity for every stage of your personal and professional life.",
+
+    "student-card-header": "Students and recent graduates",
+    "student-card-p": "We help high school students choose university studies, undergraduates select advanced courses, and recent graduates align career choices with personality and aspirations.",
+
+    "professional-card-header": "Professionals and workers",
+    "professional-card-p": "We help individuals harmonize and balance the two spheres of life, personal and professional.",
+    "professional-card-li-1": "Emotional and psychological support for managing family, children, and parents.",
+    "professional-card-li-2": "Support for professional orientation, career transitions, job changes or searches, development of new skills, improvement of work relationships, and legal issues.",
+    "professional-card-li-3": "Programs and initiatives aimed at maintaining psycho-physical well-being individually and in groups.",
+
+    "manager-card-header": "Managers",
+    "manager-card-p": "For us, people management is an art. We help current or aspiring managers gain awareness and understand how to get the best out of themselves and others.",
+
+    "companiy-card-header": "Companies",
+    "companiy-card-p": "From the individual to the team, towards greater engagement and bigger results.",
+    "companiy-card-li-1": "Through tested, validated, and constantly evolving assessment tools.",
+    "companiy-card-li-2": "By integrating new technologies with targeted and personalized meetings.",
+    "companiy-card-li-3": "By providing timely feedback to the person, the team, and the company, to achieve common goals.",
+    "companiy-card-li-4": "With regular follow-ups and checks, carried out with consistency, attention, and care.",
+    "companiy-card-li-5": "Always keeping listening channels active, to ensure timely and effective interventions.",
+
+    "approach-header": "Our Approach",
+    "approach-under-header": "Our Approach",
+
+    "listening-header": "Listening",
+    "listening-p": "We read introductory emails, listen to initial phone calls, and organize video calls to identify individual needs.",
+
+    "assesment-header": "Assessment",
+    "assesment-p": "We analyze the information, define the needs, and propose the intervention, trying to make it brief, effective, and sustainable.",
+
+    "support-header": "Support",
+    "support-p": "We start the intervention program, monitor progress, make changes if needed, and finish when the person declares themselves ready to handle situations autonomously.",
+
+    "contact-us-btn": "Contact Us",
+
+    "footer-desc": "Competent humanity for every stage of your personal and professional life.",
+
+    "footer-links": "Links",
+    "footer-links-home": "Home",
+    "footer-links-services": "Services",
+    "footer-links-about": "About us",
+
+    "footer-contact-header": "Contact Us",
+    "footer-contact-phone": "Phone: +39 123 456 7890",
+
+    "copyright": "© 2025 PL Personal Development. All rights reserved.",
+    
+    "about-header": "About Us",
+    "about-p": "People dedicated to the development of other people.",
+
+    "join-header": "We are people like you.",
+    "join-1": "People who have to make choices.",
+    "join-2": "People who go through difficult times.",
+    "join-3": "Who face fears, doubts, and challenges, becoming resilient.",
+    "join-4": "Who transform crises into opportunities, and from them forge new identities.",
+    "join-5": "People who listen to understand, to support.",
+    "join-6": "Professionals from different sciences in service of humanity, to build a resilient and supportive society.",
+
+    "contact-us-header": "Contact us",
+    "about-phone": "Phone",
+  },
+
+  it: {
+    "nav-home-btn": "Home",
+    "nav-service-btn": "Servizi",
+    "nav-about-btn": "Chi siamo",
+
+    "hero-1": "Per i momenti facili ci sono in tanti.",
+    "hero-2": "Per i momenti di sfida ci siamo noi.",
+    "hero-3": "Competente umanità per ogni fase della vita personale e professionale.",
+    "hero-4": "people, persons, needs",
+    "hero-5": "Mai più soli.",
+    "hero-6": "Scopri i nostri servizi",
+
+    "services-header": "I nostri servizi",
+    "services-under-header": "Competente umanità per ogni fase della vita personale e professionale.",
+
+    "student-card-header": "Studenti e neolaureati",
+    "student-card-p": "Aiutiamo studenti delle scuole superiori a scegliere gli studi universitari, laureandi a selezionare corsi avanzati, e neolaureati ad allineare le scelte di carriera con personalità e aspirazioni.",
+
+    "professional-card-header": "Professionisti e lavoratori",
+    "professional-card-p": "Aiutiamo gli individui ad armonizzare e bilanciare le due sfere della vita, personale e professionale",
+    "professional-card-li-1": "Supporto emotivo e psicologico per la gestione di famiglia, figli e genitori",
+    "professional-card-li-2": "Supporto di orientamento professionale, transizioni di carriera, cambio o ricerca di lavoro, sviluppo di nuove competenze, miglioramento delle relazioni al lavoro e questioni legali",
+    "professional-card-li-3": "Programmi e iniziative volte al mantenimento del benessere psico-fisico in modo individuale e gruppale",
+
+    "manager-card-header": "Manager",
+    "manager-card-p": "La gestione delle persone è per noi un'arte. Aiutiamo i manager attuali o aspiranti ad acquisire consapevolezza e capire come ottenere il meglio di sé e degli altri.",
+
+    "companiy-card-header": "Aziende",
+    "companiy-card-p": "Dal singolo al team, verso maggior engagement e più grandi risultati.",
+    "companiy-card-li-1": "Attraverso strumenti di assessment testati, validati e in costante evoluzione",
+    "companiy-card-li-2": "Integrando le nuove tecnologie con incontri mirati e personalizzati",
+    "companiy-card-li-3": "Fornendo feedback puntuali alla persona, al team e all'azienda, per il raggiungimento degli obiettivi comuni",
+    "companiy-card-li-4": "Con follow-up e verifiche regolari, svolti con costanza, attenzione e cura",
+    "companiy-card-li-5": "Mantenendo sempre attivi i canali di ascolto, per garantire interventi tempestivi ed efficaci",
+
+    "approach-header": "Il nostro approccio",
+    "approach-under-header": "Attento, sostenibile, umano.",
+
+    "listening-header": "Ascolto",
+    "listening-p": "Leggiamo le e-mail introduttive, ascoltiamo le prime telefonate e organizziamo delle videochiamate per individuare le esigenze individuali.",
+
+    "assesment-header": "Valutazione",
+    "assesment-p": "Analizziamo le informazioni, definiamo i bisogni e proponiamo l’intervento, cercando di farlo breve, efficace e sostenibile.",
+
+    "support-header": "Supporto",
+    "support-p": "Iniziamo il programma di interventi, monitoriamo il progresso, apportiamo modifiche se occorre e terminiamo quando la persona si dichiara pronta a gestire le situazioni in modo autonomo.",
+
+    "contact-us-btn": "Contattaci",
+
+    "footer-desc": "Sviluppiamo persone resilienti e team coesi attraverso valutazioni psicometriche e supporto continuo per la crescita personale e professionale.",
+
+    "footer-links": "Link Utili",
+    "footer-links-home": "Home",
+    "footer-links-services": "Servizi",
+    "footer-links-about": "Chi siamo",
+
+    "footer-contact-header": "Connettiti con Noi",
+    "footer-contact-phone": "Telefono: +39 123 456 7890",
+
+    "copyright": "© 2025 PL Personal Development. Tutti i diritti riservati.",
+    
+    "about-header": "Chi siamo",
+    "about-p": "Persone che si dedicano allo sviluppo di altre persone.",
+
+    "join-header": "Siamo persone come voi.",
+    "join-1": "Persone che devono fare delle scelte.",
+    "join-2": "Persone che attraversano momenti difficili.",
+    "join-3": "Che affrontano paure, dubbi e sfide diventando resilienti.",
+    "join-4": "Che trasformano le crisi in opportunità, e da esse forgiano nuove identità.",
+    "join-5": "Persone che ascoltano per comprendere, per sostenere.",
+    "join-6": "Professionisti di diverse scienze al servizio dell'umanità, per costruire una società resiliente e solidale.",
+
+    "contact-us-header": "Connettiti con noi",
+    "about-phone": "Telefono",
+  },
+  bg: {
+    "nav-home-btn": "Начало",
+    "nav-service-btn": "Услуги",
+    "nav-about-btn": "За нас",
+
+    "hero-1": "За лесните моменти има мнозина.",
+    "hero-2": "За предизвикателните моменти сме ние.",
+    "hero-3": "Компетентна човечност за всеки етап от Вашия личен и професионален живот.",
+    "hero-4": "people, persons, needs",
+    "hero-5": "Никога повече сами.",
+    "hero-6": "Открийте нашите услуги ",
+
+    "services-header": "Нашите услуги",
+    "services-under-header": "Компетентна човечност за всеки етап от Вашия личен и професионален живот.",
+
+    "student-card-header": "Студенти и наскоро завършили",
+    "student-card-p": "Помагаме на гимназисти да изберат университетско образование, на студенти да изберат магистърски програми и на наскоро завършили да съгласуват избора си на кариера с личността и стремежите си.",
+
+    "professional-card-header": "Професионалисти и работещи",
+    "professional-card-p": "Помагаме на хората да хармонизират и балансират двете сфери на живота – лична и професионална.",
+    "professional-card-li-1": "Емоционална и психологическа подкрепа за управление на семейството, децата и родителите.",
+    "professional-card-li-2": "Подкрепа за професионална ориентация, кариерни преходи, смяна или търсене на работа, развитие на нови умения, подобряване на работните взаимоотношения и правни въпроси.",
+    "professional-card-li-3": "Програми и инициативи, насочени към поддържане на психо-физическото благополучие индивидуално и в групи.",
+
+    "manager-card-header": "Мениджъри",
+    "manager-card-p": "За нас управлението на хора е изкуство. Помагаме на настоящи или бъдещи мениджъри да придобият осъзнатост и да разберат как да извлекат най-доброто от себе си и от другите.",
+
+    "companiy-card-header": "Компании",
+    "companiy-card-p": "От индивида до екипа, към по-голяма ангажираност и по-големи резултати.",
+    "companiy-card-li-1": "Чрез тествани, валидирани и постоянно развиващи се инструменти за оценка.",
+    "companiy-card-li-2": "Чрез интегриране на нови технологии с целенасочени и персонализирани срещи.",
+    "companiy-card-li-3": "Чрез предоставяне на навременна обратна връзка на човека, екипа и компанията за постигане на общи цели.",
+    "companiy-card-li-4": "С редовни последващи действия и проверки, извършвани с последователност, внимание и грижа.",
+    "companiy-card-li-5": "Винаги поддържайки активни канали за изслушване, за да се гарантират навременни и ефективни интервенции.",
+
+    "approach-header": "Нашият подход",
+    "approach-under-header": "Внимателен, икономически издръжлив, човечен.",
+
+    "listening-header": "Изслушване",
+    "listening-p": "Четем уводни имейли, слушаме първоначални телефонни разговори и организираме видео разговори, за да идентифицираме индивидуалните нужди.",
+
+    "assesment-header": "Оценка",
+    "assesment-p": "Анализираме информацията, дефинираме нуждите и предлагаме интервенция, като се стремим тя да бъде кратка, ефективна и устойчива.",
+
+    "support-header": "Подкрепа",
+    "support-p": "Започваме програмата за интервенции, следим напредъка, правим промени при необходимост и приключваме, когато човекът се обяви за готов да се справя със ситуациите самостоятелно.",
+
+    "contact-us-btn": "Свържете се с нас",
+
+    "footer-desc": "Компетентна човечност за всеки етап от Вашия личен и професионален живот.",
+
+    "footer-links": "Линкове",
+    "footer-links-home": "Начало",
+    "footer-links-services": "Услуги",
+    "footer-links-about": "За нас",
+
+    "footer-contact-header": "Свържете се с нас",
+    "footer-contact-phone": "Телефон: +39 123 456 7890",
+
+    "copyright": "© 2025 PL Personal Development. Всички права запазени.",
+    
+    "about-header": "За нас",
+    "about-p": "Хора, посветени на развитието на други хора.",
+
+    "join-header": "Ние сме хора като вас.",
+    "join-1": "Ние сме хора като вас.",
+    "join-2": "Хора, които преминават през трудни моменти.",
+    "join-3": "Които се изправят пред страхове, съмнения и предизвикателства, ставайки устойчиви.",
+    "join-4": "Които превръщат кризите във възможности и от тях изковават нови идентичности.",
+    "join-5": "Хора, които слушат, за да разберат, за да подкрепят.",
+    "join-6": "Професионалисти от различни науки в служба на човечеството, за да изградим устойчиво и солидарно общество.",
+
+    "contact-us-header": "Свържете се с нас",
+    "about-phone": "Телефон",
+  },
+};
